ece4e3421df500b253988d5b93ef3352f25e7259
Mayi Echeveste <mayi.echeveste@opendeusto.es>
2019-12-02 10:54:35 +0100

    gestor Eventos modificado

--- src/main/java/gestor/gestorEventos.java
+++ src/main/java/gestor/gestorEventos.java
@@ -1,16 +1,35 @@
 package gestor;
 
+import java.util.List;
+
 import dao.DB_DAO_E;
-import data.*;
+import data.Event;
 
 public class gestorEventos 
 {
 
 	public void crearEvento(Event evento) 
 	{
+	//	Event objEvento = new Event();
 		DB_DAO_E objDao = new DB_DAO_E();
 		
 		objDao.registrarE(evento);
+		
+		
+	}
+	
+	public void eliminarEvento(int idEvent)
+	{
+		
+		//eliminar evento
+	}
+	
+	public List<Event> listaEventos() 
+	{
+		return null;
+		// sacar mediante DB_DAO_E lista de todos los eventos ==> metodo en dao todavia no esta
+		
+		
 	}
 	
 }
