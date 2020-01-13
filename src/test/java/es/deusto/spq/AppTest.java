995fb799aa45d5931bceae1451d0be16ea070aae
Mayi Echeveste <mayi.echeveste@opendeusto.es>
2020-01-13 11:30:17 +0100

    Test unitario + jacoco para coverage, las dependencias ya estan en el pom subido cmd-> mvn compile y mvn test. Os genera la carpeta target con los informes

--- src/test/java/es/deusto/spq/AppTest.java
+++ src/test/java/es/deusto/spq/AppTest.java
@@ -2,7 +2,8 @@ package es.deusto.spq;
 
 import static org.junit.Assert.assertTrue;
 
-import java.sql.Date;
+import java.util.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.junit.After;
@@ -17,22 +18,25 @@ import gestor.gestorEventos;
 
 public class AppTest {
 	
-	 @Before
+	/* @Before
 	    public void setUp() {
 	    	final gestorEspacios gespacios = new gestorEspacios();
 			final gestorEventos geventos = new gestorEventos();
 			
 	    }
+	 
+	 
 	 @Ignore("Utilizar cuando pongamos tests sobre bbdd")
 	 @After
 	    public void tearDown()
 	    {
 	        //eliminar espacios y eventos creados en tests
 	    }
-	
+	*/
+	@Ignore("Pruebo a ignorar")
 	@Test
     public void crearObjNoPersistentes() {
-        Spaces objEspacio = new Spaces(2,"bbklive","bilbao",100000,"");
+        Spaces objEspacio = new Spaces("bbklive","bilbao",100000,"recurso1");
         
         Date date;
         
@@ -45,18 +49,38 @@ public class AppTest {
         		date = null;
         	}
        
-		Event evento = new Event(0, "bbklive", "music", 50, "campside", date, 5, 0);
+		Event evento = new Event("bbklive", "music", 50, "campside", date, 5, 0);
 
     }
 	
-	@Ignore("Para testear base de datos")
+	//@Ignore("Para testear base de datos")
 	@Test
-	public void bdtest()
+	public void bdtestCrear()
 	{
- 
-		//gespacios.crearEspacio(objEspacio);
-		//geventos.crearEvento(evento);
-		//gespacios.eliminarEspacio(1);
+		gestorEspacios objGestorEspacios = new gestorEspacios();
+    	gestorEventos objGestorEventos= new gestorEventos();
+    	    	
+    	
+    	Spaces s1 = new Spaces("primero","bilbao",100000,"recursoA");
+    	Spaces s2= new Spaces("segundo","donosti",50000,"recursoB");
+    			
+    	objGestorEspacios.crearEspacio(s1);
+    	objGestorEspacios.crearEspacio(s2);
+    	
+    	
+    	Date date = null;
+		try {
+			date =new SimpleDateFormat("yyyy-MM-dd").parse("2014-02-14");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Event e1 = new Event("primero", "music", 50, "campside", date, 5, 0);
+		Event e2 = new Event("segundo", "teatro", 15, "nose", date, 8, 0);
+		
+		objGestorEventos.crearEvento(e1,s1.getIdSpace());
+		objGestorEventos.crearEvento(e2,s1.getIdSpace());
 	}
 	
 }
